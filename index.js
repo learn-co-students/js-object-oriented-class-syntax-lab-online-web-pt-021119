ecbf6cf4cb983abca09f76f8f5a1b17b281fb7f6
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-09-19 00:34:01 -0700

    Done.

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ class BoardMember{
     this.homeState = homeState;
     this.training = training;
   };
-}
 
-BoardMember.prototype.veto = function(){
-  return "No, I must disagree";
-}
+  veto(){
+    return "No, I must disagree";
+  }
 
-BoardMember.prototype.approve = function(){
-  return "You can do that!";
-}
+  approve(){
+    return "You can do that!";
+  }
 
-BoardMember.prototype.doCharity = function(){
-  return "I like to help people."
-}
+  doCharity(){
+    return "I like to help people."
+  }
 
-BoardMember.prototype.releasePressStatement = function(){
-  return "You will see great things from Scuber.";
-}
+  releasePressStatement(){
+    return "You will see great things from Scuber.";
+  }
 
-BoardMember.prototype.sayHi = function(){
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
 };
 
 class Ceo extends BoardMember{
